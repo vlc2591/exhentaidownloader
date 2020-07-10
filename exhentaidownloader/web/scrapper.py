2c9fb86727b48b04733a47825fc13d8bdee0dab3
vlc2591 <66829585+vlc2591@users.noreply.github.com>
2020-07-10 13:10:32 +0200

    Added new tests and get_title method

--- exhentaidownloader/web/scrapper.py
+++ exhentaidownloader/web/scrapper.py
@@ -1,5 +1,8 @@
 from requests import Response
+import re
 
+p_first_page_gallery = re.compile(r'<a href=\"(https\:\/\/exhentai\.org\/s\/\w+\/\w+\-1)\">')
+p_gallery_title = re.compile(r'<h1 id="gn">(.*)<\/h1><h1 id="gj">')
 
 def get_first_image_page_link(data: Response):
     return ""
@@ -11,3 +14,12 @@ def get_next_image_page_link(data: Response):
 
 def get_image_link(data: Response):
     return ""
+
+
+def get_gallery_title(data: Response):
+    match = p_gallery_title.search(data.text)
+    if type(match) is re.Match:
+        result = match.groups()[0]
+    else:
+        result = ""
+    return result
--- tests/web/test_scrapper.py
+++ tests/web/test_scrapper.py
@@ -1,6 +1,7 @@
 import os
 
-from exhentaidownloader.web.scrapper import get_first_image_page_link, get_next_image_page_link, get_image_link
+from exhentaidownloader.web.scrapper import get_first_image_page_link, get_next_image_page_link, get_image_link, \
+    get_gallery_title
 from tests.web.mock_response import MockResponse
 
 test_files_path = os.path.abspath(os.path.dirname(__file__))
@@ -8,7 +9,7 @@ test_files_path = os.path.abspath(os.path.dirname(__file__))
 
 class TestScrapper:
     def test_retrieve_link_for_first_image_from_gallery(self):
-        with open(f'{test_files_path}/gallery.html') as file:
+        with open(f'{test_files_path}/gallery.html', 'rb') as file:
             data = file.read()
         mock_response = MockResponse(data, 200)
         expected_link = 'https://exhentai.org/s/6f365a4f9b/574260-1'
@@ -16,7 +17,7 @@ class TestScrapper:
         assert link == expected_link
 
     def test_retrieve_link_for_next_image_from_image_page(self):
-        with open(f'{test_files_path}/image_middle_gallery.html') as file:
+        with open(f'{test_files_path}/image_middle_gallery.html', 'rb') as file:
             data = file.read()
         mock_response = MockResponse(data, 200)
         expected_link = 'https://exhentai.org/s/ebc9f3feb8/574260-6'
@@ -24,7 +25,7 @@ class TestScrapper:
         assert link == expected_link
 
     def test_retrieve_link_for_actual_image_from_image_page(self):
-        with open(f'{test_files_path}/image_middle_gallery.html') as file:
+        with open(f'{test_files_path}/image_middle_gallery.html', 'rb') as file:
             data = file.read()
         mock_response = MockResponse(data, 200)
         expected_link = 'https://gvvrlvv.gsvxzhxipxzi.hath.network/h/d9718fe632141993150f01daf36485c0e479114b-' \
@@ -32,3 +33,19 @@ class TestScrapper:
                         'fc6280bc2fc82bfe1b0690bed6.jpg'
         link = get_image_link(mock_response)
         assert link == expected_link
+
+    def test_get_title(self):
+        with open(f'{test_files_path}/gallery.html', 'rb') as file:
+            data = file.read()
+        mock_response = MockResponse(data, 200)
+        expected_title = 'High Heels'
+        title = get_gallery_title(mock_response)
+        assert title == expected_title
+
+    def test_get_title_with_japanese_one_is_present(self):
+        with open(f'{test_files_path}/gallery_with_japanese_title.html', 'rb') as file:
+            data = file.read()
+        mock_response = MockResponse(data, 200)
+        expected_title = '[hentaiworks (Aruma)] Futanari Sennou Kaizou Koujou 2/4 [Chinese]'
+        title = get_gallery_title(mock_response)
+        assert title == expected_title
