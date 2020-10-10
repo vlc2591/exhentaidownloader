ebb931e3ef50a36e3c06ce9688253556c77baa3d
vlc2591 <66829585+vlc2591@users.noreply.github.com>
2020-10-10 16:42:18 +0200

    Few test declaration

---
+++ tests/processor/test_gallery.py
@@ -0,0 +1,7 @@
+class TestGalleryProcessor:
+
+    def test_raise_exception_if_not_given_gallery_link_in_constructor(self):
+        raise Exception
+
+    def test_function_process_called_and_ok(self):
+        raise Exception
