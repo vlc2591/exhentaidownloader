5c0125c497dfe1cbc75d125d851df81301ab4171
vlc2591 <66829585+vlc2591@users.noreply.github.com>
2020-10-10 16:56:18 +0200

    Added scrapping of total images in gallery
    For progress info purposes

--- exhentaidownloader/web/scrapper.py
+++ exhentaidownloader/web/scrapper.py
@@ -5,6 +5,7 @@ p_first_page_gallery = re.compile(r'<a href=\"(https\:\/\/exhentai\.org\/s\/\w+\
 p_gallery_title = re.compile(r'<h1 id="gn">(.*)<\/h1><h1 id="gj">')
 p_next_image_page = re.compile(r'<a id=\"next\" onclick=\"return load_image\(\d, \'\w+\'\)\" href=\"(https\S+)\"><img')
 p_image_link = re.compile(r'<img id=\"img\" src=\"(\S+)\" style')
+p_gallery_total_images = re.compile(r'<td class="gdt2">(\d+) pages</td>')
 
 
 def get_first_image_page_link(data: Response):
@@ -21,6 +22,10 @@ def get_image_link(data: Response):
 
 def get_gallery_title(data: Response):
     return __regex_search(p_gallery_title, data.text)
+
+
+def get_number_of_images(data: Response):
+    return int(__regex_search(p_gallery_total_images, data.text))
 
 
 def __regex_search(pattern: re.Pattern, text: str):
